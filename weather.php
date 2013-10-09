b71e8182aff78be0fc86ae04dfc2a7473a4f609c
Paul Reed <pauldreed@gmail.com>
2013-10-09 21:22:09 +0100

    Update weather.php

--- weather.php
+++ weather.php
@@ -5,32 +5,33 @@
 //Documentation - http://www.wunderground.com/weather/api/d/documentation.html
 
 //Change the weather underground API and city below
-$json_string = file_get_contents("http://api.wunderground.com/api/XXXXX-My-API-Key-XXXXX/conditions/q/pws:ICANTLEY2.json");
+$json_string = file_get_contents("http://api.wunderground.com/api/your-api-key/conditions/q/pws:IFINNING3.json");
 $parsed_json = json_decode($json_string);
 
 $location = $parsed_json->{'current_observation'}->{'display_location'}->{'city'};
-$relative_humidity = $parsed_json->{'current_observation'}->{'relative_humidity'};
-$pressure_mb = $parsed_json->{'current_observation'}->{'pressure_mb'};
+$humidity = $parsed_json->{'current_observation'}->{'relative_humidity'};
+$pressure = $parsed_json->{'current_observation'}->{'pressure_mb'};
 $wind_mph = $parsed_json->{'current_observation'}->{'wind_mph'};
-$wind_gust_mph = $parsed_json->{'current_observation'}->{'wind_gust_mph'};
+$wind_g_mph = $parsed_json->{'current_observation'}->{'wind_gust_mph'};
+$rain_hr = $parsed_json->{'current_observation'}->{'precip_1hr_metric'};
+$rain_today = $parsed_json->{'current_observation'}->{'precip_today_metric'};
 
-//Get and save the icon_url to /images
+//Get and save the icon_url to /pictures
 $image_url = $parsed_json->{'current_observation'}->{'icon_url'};
-//Change the required location of downloaded image file
 $current_con = '/var/www/images/current_con.gif';
 file_put_contents($current_con, file_get_contents($image_url));
 
-echo "\nCurrent relative humidity in ${location} is: ${relative_humidity}\n";
-//Can duplicate the above echo statement for wind, pressure, etc. if needed
-
+//The following line can be used to screen print the variables 
+//echo "\nCurrent relative humidity in ${location} is: ${humidity}\n";
+ 
 //Change the URL and EmonCMS API below
-$url = 'http://yourURL/emoncms/api/post?apikey=XXXXX-My-API-Key-XXXXX&json={humidity:' . $relative_humidity . ',pressure:' . ',wind:' . $wind_mph . ',windgust:' . $wind_gust_mph .'}'; $pressure_mb . '$
+$url = 'http://url/emoncms/api/post?apikey=your-emoncms-api-key&json={humidity:' . $humidity . ',pressure:' . $pressure . ',wind:' . $wind_mph . ',windgust:' . $wind_g_mph . ',rain_today:' . $rain_today . ',rain_hr:' . $rain_hr .'}';
 echo $url;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $contents = curl_exec ($ch);
 curl_close ($ch);
-
+ 
 ?>
 </html>
